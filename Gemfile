c466119cf73f9b129453bf76826a470ee9f5f0c0
Eric James <eric.james@yale.edu>
2024-09-12 18:46:30 +0000

    CASify production 10.4.34.110 commit

--- Gemfile
+++ Gemfile
@@ -83,7 +83,7 @@ gem 'devise'
 #https://github.com/yalelibrary/yul-dc-blacklight/blob/main/Gemfile
 gem 'omniauth', '~> 1.9.2'
 gem 'omniauth-cas'
-gem 'omniauth-rails_csrf_protection', '~> 0.1'
+#gem 'omniauth-rails_csrf_protection', '~> 0.1'
 
 gem 'sorted_set'
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
 
+  #replaced this with CAS auth
   #y = YAML.load_file("#{Rails.root.to_s}/config/solr.yml")
   #user1 = y["user1"]
   #password1 = y["password1"]
   #http_basic_authenticate_with name: user1, password: password1
+  
+  #tried these to no avail, instead removed CSRF gem
+  ##skip_before_action :verify_authenticity_token
+  #protect_from_forgery prepend: true, with: :exception
+  ##protect_from_forgery prepend: true, with: :null_session
 
   before_action :authenticate_user!
 
