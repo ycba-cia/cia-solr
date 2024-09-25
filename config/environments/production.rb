09c8eebd4709ec4191ad08d7ea39317b54c58d9a
Eric James <eric.james@yale.edu>
2024-09-25 17:05:32 +0000

    turn on csrf, remove logout link

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_controller.forgery_protection_origin_check = false
+  config.action_controller.forgery_protection_origin_check = true
   # Code is not reloaded between requests.
   config.cache_classes = true
 
