0bc7b0a3c604909dd108d582669cab6f26daafd3
Eric James <eric.james@yale.edu>
2024-11-19 13:47:15 -0500

    error handling in new and submit

--- app/controllers/frames_controller.rb
+++ app/controllers/frames_controller.rb
@@ -32,11 +32,55 @@ class FramesController < ApplicationController
 
   def submit
     #if params["solr_id"].present?
-    @objectID = params["objectID"]
-    @objectNumber = params["objectNumber"]
-    @frameObjectID = params["frameObjectID"]
-    @frameObjectNumber = params["frameObjectNumber"]
+    objectID = params["objectID"]
+    objectNumber = params["objectNumber"]
+    frameObjectID = params["frameObjectID"]
+    frameObjectNumber = params["frameObjectNumber"]
+
+    @errormsg = ""
+    if objectID.length == 0 || objectNumber.length == 0 || frameObjectID.length == 0 || frameObjectNumber.length == 0
+      @errormsg += "<p>All fields must be populated.</p>"
+    end
+    unless objectID.to_i.to_s == objectID
+      @errormsg += "<p>ObjectID must be an integer.</p>"
+    end
+    unless frameObjectID.to_i.to_s == frameObjectID
+      @errormsg += "<p>FrameObjectID must be an integer.</p>"
+    end
+    if @errormsg.length == 0
+      Frame.where(objectID: objectID).update_all(frameObjectID: frameObjectID, frameObjectNumber: frameObjectNumber)
+      f = Frame.where(objectID: objectID).first
+      @objectID= f.ObjectID
+      @objectNumber = f.ObjectNumber
+      @frameObjectID = f.FrameObjectID
+      @frameObjectNumber = f.FrameObjectNumber
+    end
+  end
+
+  def new
+    #if params["solr_id"].present?
+    objectID = params["objectID"]
+    objectNumber = params["objectNumber"]
+    frameObjectID = params["frameObjectID"]
+    frameObjectNumber = params["frameObjectNumber"]
+
+    @errormsg = ""
+    if objectID.length == 0 || objectNumber.length == 0 || frameObjectID.length == 0 || frameObjectNumber.length == 0
+      @errormsg += "<p>All fields must be populated.</p>"
+    end
+    unless objectID.to_i.to_s == objectID
+      @errormsg += "<p>ObjectID must be an integer.</p>"
+    end
+    unless frameObjectID.to_i.to_s == frameObjectID
+      @errormsg += "<p>FrameObjectID must be an integer.</p>"
+    end
+    if @errormsg.length == 0
+    #case sensitive
+      f = Frame.create(ObjectID: objectID, ObjectNumber: objectNumber, FrameObjectID: frameObjectID, FrameObjectNumber: frameObjectNumber)
+      @objectID= f.ObjectID
+      @objectNumber = f.ObjectNumber
+      @frameObjectID = f.FrameObjectID
+      @frameObjectNumber = f.FrameObjectNumber
+    end
   end
 end
-
-
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'frames/index'
   get 'frames/confirm', to: "frames#confirm"
   get 'frames/submit', to: "frames#submit"
+  get 'frames/new', to: "frames#new"
 
   #ERJ 9/11/2024 added devise_for
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
