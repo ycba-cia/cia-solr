7d49d2d57c8d367f27315e2ee0eb60143dbf28b8
Eric James <eric.james@yale.edu>
2025-12-01 12:56:46 -0500

    updated clan_blacklight.rb from drupal

--- scripts/blacklight_clean/clean_blacklight.rb
+++ scripts/blacklight_clean/clean_blacklight.rb
@@ -1,29 +1,48 @@
 require 'rsolr'
-solr_url1 = "http://10.5.96.187:8983/solr/ycba-collections_dev1"
-solr_url2 = "https://user:6bELqBB8UTfl@ciaindex.britishart.yale.edu/solr/ycba_blacklight"
+require 'yaml'
+
+y = YAML.load_file("../../config/solr.yml")
+
+solr_url1 = y["url"]
+#solr_url2 = "https://user:****@ciaindex.britishart.yale.edu/solr/ycba_blacklight"
 
 solr_conn1 = RSolr.connect :url => solr_url1
-solr_conn2 = RSolr.connect :url => solr_url2
+#solr_conn2 = RSolr.connect :url => solr_url2
 
 #uncomment to delete
 #mode = "delete"
+output = "list"
 
+fq = 'collection_ss:"Reference Library" && timestamp_dt:[* TO NOW-1DAY]'
+#fq = 'collection_ss:"Rare Books and Manuscripts" && timestamp_dt:[* TO NOW-1DAY]'
 response = solr_conn1.post 'select', :params => {
-          #:fq=>'object_type_s:"scan"',
+          :fq =>fq,
           :fl=>'id,timestamp_dt,title_ss,author_ss,collection_ss',
-          :sort=>'timestamp_dt asc',
+          :sort=>'timestamp_dt desc',
           :start=>0,
-          :rows=>1000,
-          :fq=> "-timestamp_dt:[NOW-8DAY TO NOW]"
+          :rows=>200000
       }
 
 docs = response['response']['docs']
+puts "fq:#{fq}"
 if docs.length == 0
   puts "no items to clean"
   exit
 else
   puts "number of documents to clean: #{docs.length}"
+  puts ""
+  if output == "list"
+    docs.each_with_index { |doc, i|
+      #break if i > 10
+      puts doc["title_ss"]
+      puts doc["id"]
+      puts doc["timestamp_dt"]
+      puts ""
+    }
+  end
 end
+
+exit
 
 docs.each { |doc|
   id = doc["id"]
@@ -32,8 +51,8 @@ docs.each { |doc|
     solr_conn1.delete_by_id id
     solr_conn1.commit
     puts "deleted from solr_conn1: #{id}"
-    solr_conn2.delete_by_id id
-    solr_conn2.commit
-    puts "deleted from solr_conn2: #{id}"
+    #solr_conn2.delete_by_id id
+    #solr_conn2.commit
+    #puts "deleted from solr_conn2: #{id}"
   end
 }
